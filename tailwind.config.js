4e4b0df23e787154aedb0c5d237370de059e2f67
AshBots <ashworldco@gmail.com>
2025-11-01 02:53:54 +0500

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+    "./public/index.html"
+  ],
+  theme: {
+    extend: {
+      animation: {
+        bounce: 'bounce 1s infinite'
+      }
+    }
+  },
+  plugins: []
+}
