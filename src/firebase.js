0a2b2f22a439d68de5b7c81f14cb30295c2321ca
AshBots <ashworldco@gmail.com>
2025-11-04 01:33:55 +0500

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,15 +1,17 @@
-import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
+import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAAFqbEIL3TOAcFmsxoqltJfrtfE2sOXVs",
-  authDomain: "links-dm-pro.firebaseapp.com",
-  projectId: "links-dm-pro",
-  storageBucket: "links-dm-pro.appspot.com",
-  messagingSenderId: "965082307073",
-  appId: "1:965082307073:web:78ea49e4c5888852307e00",
-  measurementId: "G-QVH0R5D92B"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
+export const auth = getAuth(app);
+export default app;
